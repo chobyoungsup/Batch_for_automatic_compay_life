4d65d8d744f85948fa3a1fb814c9e8a1c63460a0
heejaena <naheejae64@gmail.com>
2021-08-29 01:14:52 +0900

    first commit

---
+++ backup_eventlog/backup_eventlog.bat
@@ -0,0 +1,9 @@
+set desDir=D:\backup\
+set todayDate=%date:~0,4%%date:~5,2%%date:~8,2%
+
+echo f | xcopy "C:\Windows\system32\winevt\Logs\Application.evtx" "%desDir%%todayDate%_Application.evtx"
+echo f | xcopy "C:\Windows\system32\winevt\Logs\Security.evtx" "%desDir%%todayDate%_Security.evtx"
+echo f | xcopy "C:\Windows\system32\winevt\Logs\System.evtx" "%desDir%%todayDate%_System.evtx"
+
+set desDir=
+set todayDate=
